8b57f4314a191b8ec93376a46802843650ecd6a7
unknown <rubainagupta@gmail.com>
2026-03-17 22:25:25 +0530

    Updated file

--- Models/Module.cs
+++ Models/Module.cs
@@ -4,46 +4,51 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace GPMS.Models;
-
-[Table("Module")]
-public partial class Module
+namespace GPMS.Models
 {
-    [Key]
-    [Column("module_id")]
-    public int ModuleId { get; set; }
+    [Table("Module")]
+    public partial class Module
+    {
+        [Key]
+        [Column("module_id")]
+        public int ModuleId { get; set; }
 
-    [Column("project_id")]
-    public int ProjectId { get; set; }
+        [Required(ErrorMessage = "Project is required")]
+        [Column("project_id")]
+        public int ProjectId { get; set; }
 
-    [Column("module_name")]
-    [StringLength(100)]
-    [Unicode(false)]
-    public string ModuleName { get; set; } = null!;
+        [Required]
+        [Column("module_name")]
+        [StringLength(100)]
+        [Unicode(false)]
+        public string ModuleName { get; set; } = string.Empty;
 
-    [Column("details")]
-    [StringLength(255)]
-    [Unicode(false)]
-    public string? Details { get; set; }
+        [Column("details")]
+        [StringLength(255)]
+        [Unicode(false)]
+        public string? Details { get; set; }
 
-    [Column("module_status")]
-    [StringLength(50)]
-    [Unicode(false)]
-    public string? ModuleStatus { get; set; }
+        [Column("module_status")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? ModuleStatus { get; set; }
 
-    [Column("module_start_date")]
-    public DateOnly? ModuleStartDate { get; set; }
+        [Column("module_start_date")]
+        public DateOnly? ModuleStartDate { get; set; }
 
-    [Column("module_end_date")]
-    public DateOnly? ModuleEndDate { get; set; }
+        [Column("module_end_date")]
+        public DateOnly? ModuleEndDate { get; set; }
 
-    [InverseProperty("Module")]
-    public virtual ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
+        // Navigation
+        [InverseProperty("Module")]
+        public virtual ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
 
-    [ForeignKey("ProjectId")]
-    [InverseProperty("Modules")]
-    public virtual Project Project { get; set; } = null!;
+        [ForeignKey("ProjectId")]
+        [InverseProperty("Modules")]
+        public virtual Project? Project { get; set; }
 
-    [InverseProperty("Module")]
-    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+        [InverseProperty("Module")]
+        public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+
+    }
 }
